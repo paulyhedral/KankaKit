c88e1632d01e1a4780e6b11acfb8d47a47637c4b
Paul Schifferer <paul@schifferers.net>
2021-01-02 22:23:13 -0800

    Add request for campaigns.

---
+++ Sources/KankaKit/Requests/Campaigns.swift
@@ -0,0 +1,23 @@
+//
+//  Campaigns.swift
+//  KankaKit
+//
+//  Created by Paul Schifferer on 4/9/17.
+//  Copyright © 2021 KankaKit. All rights reserved.
+//
+
+import Foundation
+
+/// `Campaign` requests.
+public enum Campaigns {
+    /// Gets the user's campaigns.
+    ///
+    /// - Returns: Request for `Campaign`.
+    public static func campaigns(range: RequestRange = .default) -> Request<[Campaign]> {
+        return Request<[Campaign]>(path: "/campaigns")
+    }
+
+    public static func campaign(id: Int) -> Request<Campaign> {
+        return Request<Campaign>(path: "/campaigns/\(id)")
+    }
+}
